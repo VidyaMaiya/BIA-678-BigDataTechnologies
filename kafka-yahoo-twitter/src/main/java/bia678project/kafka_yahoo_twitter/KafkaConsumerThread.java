4f1968dd870849dc844651eabdbebf7e0a99ba1b
VidyaMaiya <vid04star@gmail.com>
2019-11-28 16:55:37 -0500

    Changed logger class

--- kafka-yahoo-twitter/src/main/java/bia678project/kafka_yahoo_twitter/KafkaConsumerThread.java
+++ kafka-yahoo-twitter/src/main/java/bia678project/kafka_yahoo_twitter/KafkaConsumerThread.java
@@ -13,7 +13,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class KafkaConsumerThread implements Runnable {
-	Logger logger = LoggerFactory.getLogger(TwitterProducer.class.getName());
+	Logger logger = LoggerFactory.getLogger(KafkaConsumerThread.class.getName());
 	private String topicName;
 	private String groupId;
 
